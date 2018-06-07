d5c6a7c8e9c4522ea8b82101eb3303ef262cb090
eason <crazy653555@gmail.com>
2018-06-07 21:54:00 +0800

    完成初稿

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,36 +1,46 @@
-import { Component, OnInit } from "@angular/core";
-import { Observable } from "rxjs";
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { async } from '@angular/core/testing';
 import {
   AngularFirestoreDocument,
   AngularFirestore
-} from "angularfire2/firestore";
+} from 'angularfire2/firestore';
 
 @Component({
-  selector: "app-home",
-  templateUrl: "./home.component.html",
-  styleUrls: ["./home.component.css"]
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
   item$: Observable<any[]>;
-  title = "app";
+  title = 'app';
   menusHandler: any;
   private itemDoc: AngularFirestoreDocument<any>;
   item: Observable<any>;
+  controll = false;
+  aaa: any;
+  // constructor(private _db: AngularFireDatabase) {
+  // this.item$ = _db.list('item').valueChanges();
+  // }
 
-  //constructor(private _db: AngularFireDatabase) {
-  //this.item$ = _db.list('item').valueChanges();
-  //}
-
-  ngOnInit(): void {}
+  ngOnInit(): void { }
 
   constructor(private afs: AngularFirestore) {
-    this.itemDoc = afs.doc<any>("items/item");
+    this.itemDoc = afs.doc<any>('items/item');
     this.item = this.itemDoc.valueChanges();
-    console.log(this.item);
+    console.log('get controll = ' + this.item);
+    this.itemDoc.update({ 'controll': this.controll });
+
+    this.aaa = this.item.subscribe();
+    console.log(this.aaa);
   }
 
   changeText() {
-    console.log("aa");
-    this.itemDoc.update({ "1": true });
+    if (this.controll) {
+      this.controll = false;
+    } else {
+      this.controll = true;
+    }
+    this.itemDoc.update({ 'controll': this.controll });
   }
 }
--- src/app/second/second.component.ts
+++ src/app/second/second.component.ts
@@ -1,35 +1,49 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import {
+  AngularFirestoreDocument,
+  AngularFirestore
+} from 'angularfire2/firestore';
 
 @Component({
-  selector: "app-second",
-  templateUrl: "./second.component.html",
-  styleUrls: ["./second.component.css"]
+  selector: 'app-second',
+  templateUrl: './second.component.html',
+  styleUrls: ['./second.component.css']
 })
 export class SecondComponent implements OnInit {
-  url: string = location.href; //取得網址
-  getPara = [];
-  aryPara = []; //存放queryString
-  setText =
-    "輸入方式:?color=#ff0000&fontsize=20em&scrollAmount=20&text=跑馬燈內容";
-  setColor = "#ff0000";
-  setFontSize = "20em";
-  setScrollAmount = "30";
 
-  constructor() {}
+  private itemDoc: AngularFirestoreDocument<any>;
+  item: Observable<any>;
+
+  url: string = location.href; // 取得網址
+  getPara = [];
+  aryPara = []; // 存放queryString
+  setText =
+    '輸入方式:?color=#ff0000&fontsize=20em&scrollAmount=20&text=跑馬燈內容';
+  setColor = '#ff0000';
+  setFontSize = '20em';
+  setScrollAmount = '30';
+
+
+  constructor(private afs: AngularFirestore) {
+    this.itemDoc = afs.doc<any>('items/item');
+    this.item = this.itemDoc.valueChanges();
+
+  }
 
   ngOnInit() {
-    //取得queryString參數
-    if (this.url.indexOf("?") != -1) {
-      this.getPara = this.url.split("?")[1].split("&");
+    // 取得queryString參數
+    if (this.url.indexOf('?') !== -1) {
+      this.getPara = this.url.split('?')[1].split('&');
 
       for (let i = 0; i < this.getPara.length; i++) {
-        this.aryPara.push(this.getPara[i].split("=")[1]);
+        this.aryPara.push(this.getPara[i].split('=')[1]);
       }
 
-      this.setColor = this.aryPara[0]; //字體顏色
-      this.setFontSize = this.aryPara[1]; //字體大小
-      this.setScrollAmount = this.aryPara[2]; //捲動速度
-      this.setText = decodeURI(this.aryPara[3]); //文字
+      this.setColor = this.aryPara[0]; // 字體顏色
+      this.setFontSize = this.aryPara[1]; // 字體大小
+      this.setScrollAmount = this.aryPara[2]; // 捲動速度
+      this.setText = decodeURI(this.aryPara[3]); // 文字
     }
   }
 }
